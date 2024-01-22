205a187df3c3954ceebccca5e65f8c6ac8780090
alnum0101 <125492798+alnum0101@users.noreply.github.com>
2024-01-22 11:26:46 +0530

    Update program05.c

--- DSA/program05.c
+++ DSA/program05.c
@@ -9,22 +9,20 @@
 int i, top = -1;
 int op1, op2, res, s[20];
 char postfix[90], symb;
-void
-push(int item) {
+
+void push(int item) {
   top = top + 1;
   s[top] = item;
 }
 
-int
-pop() {
+int pop() {
   int item;
   item = s[top];
   top = top - 1;
   return item;
 }
 
-void
-main() {
+void main() {
   printf("\nEnter a valid postfix expression:\n");
   scanf("%s", postfix);
   for (i = 0; postfix[i] != '\0'; i++) {
