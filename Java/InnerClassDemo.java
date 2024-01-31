eb686f9d1ca1a051c3366a42c16ff888bd55a55e
alnum0101 <125492798+alnum0101@users.noreply.github.com>
2024-01-31 11:37:43 +0530

    Add files via upload

---
+++ Java/InnerClassDemo.java
@@ -0,0 +1,21 @@
+class Outer {
+  int x = 100;
+  class Inner {
+    void display() {
+      System.out.println("Inner_display: x=" + x);
+    }
+  } //end Inner
+
+  void display() {
+    System.out.println("Outer_display:x=" + x);
+    Inner in = new Inner();
+    in.display();
+  }
+} //end Outer
+
+class InnerClassDemo {
+  public static void main(String args[]) {
+    Outer out = new Outer();
+    out.display();
+  }
+} //end Demo
